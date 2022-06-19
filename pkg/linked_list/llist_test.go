21d3ad15a7bc046a75176c217b1a2a2178fbba38
ZiXyos <killian.moudery@gmail.com>
2022-06-19 23:45:17 +0200

    test: add back mock test

--- pkg/linked_list/llist_test.go
+++ pkg/linked_list/llist_test.go
@@ -6,12 +6,24 @@ import (
 
 func AddElemAtFront_T(t *testing.T) {
 
-	list := List{}
-	if &list == nil {
+	list := &List{}
+	if list == nil {
 
 		t.Error("list not created")
 	}
 
 	list.AddElemAtFront("1st injected")
 	list.AddElemAtFront("2nd injected")
+}
+
+func AddElemtAtBack_T(t *testing.T) {
+
+	list := &List{}
+	if list == nil {
+
+		t.Error("lost not created")
+	}
+
+	list.AddElemtAtBack("1st")
+	list.AddElemtAtBack("2nd")
 }
